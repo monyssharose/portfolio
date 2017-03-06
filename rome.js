a3227a84fb83c4e089f4e9d95db91f4187344eec
monyssharose <monyssharose@unm.edu>
2017-03-06 15:32:06 -0700

    Update rome.js

--- rome.js
+++ rome.js
@@ -88,14 +88,12 @@
 		]
 	}
 ]
-
-			var map;
-			function initMap() {
-				map = new google.maps.Map(document.getElementById('map'), {
-					center: {lat: 41.890333, lng: 12.491887},
-					zoom: 13,
-					mayTypeId: google.maps.MapTypeId.HYBRID,
-					draggable: false,
-					scrollwheel: false
-				});
-			}
+var map;
+	function initMap() {
+		map = new google.maps.Map(document.getElementById('map'), {
+			center: {lat: 41.890333, lng: 12.491887},
+			zoom: 13,
+			draggable: false,
+			scrollwheel: false
+			});
+	}

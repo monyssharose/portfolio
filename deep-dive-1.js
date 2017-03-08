ea2017c8a547eb5aa99f2d6c0543b80964dcc883
monyssharose <monyssharose@unm.edu>
2017-03-08 13:07:21 -0700

    Create deep-dive-1.js

---
+++ deep-dive-1.js
@@ -0,0 +1,68 @@
+function initMap () {
+	var map = new google.maps.Map(document.getElementById('map'), {
+		center: {lat: 41.890333, lng: 12.491887},
+		zoom: 13,
+		draggable: false,
+		scrollwheel: false,
+		styles: [
+			{
+    	"featureType": "administrative.country",
+    	"stylers": [
+    	  {
+        "color": "#d9ccb9"
+     		 }
+   		 ]
+  		},
+  		{
+  	  "featureType": "landscape.man_made",
+  	  "stylers": [
+   		  {
+     	  "color": "#d4d4d4"
+  			 }
+  	 		]
+  		},
+  		{
+    	"featureType": "poi.park",
+   	 "stylers": [
+      {
+        "color": "#95cca3"
+      }
+    ]
+  },
+  {
+    "featureType": "road.arterial",
+    "elementType": "labels",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "labels",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "road.local",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "stylers": [
+      {
+        "color": "#2fb6ff"
+      }
+    ]
+  }
+]
+	});
+}

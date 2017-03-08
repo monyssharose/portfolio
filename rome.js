89bb1ba8267f47d7a6a3a6ef4a19305b52a4ada4
monyssharose <monyssharose@unm.edu>
2017-03-07 17:56:02 -0700

    Update rome.js

--- rome.js
+++ rome.js
@@ -1,6 +1,6 @@
 var map;
 function initMap() {
-	map = new google.maps.Map(document.getElementById('romemap'), {
+	map = new google.maps.Map(document.getElementById('map'), {
 		center: {lat: 41.890333, lng: 12.491887},
 		zoom: 13,
 		draggable: false,

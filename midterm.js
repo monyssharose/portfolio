40a03be5bc7b8e1f06ca0f0cb8e1d8c3d44612b0
monyssharose <monyssharose@unm.edu>
2017-03-10 08:12:12 -0700

    Update midterm.js

--- midterm.js
+++ midterm.js
@@ -4,8 +4,9 @@ function initMap(){
  var map = new google.maps.Map(document.getElementById('map'), {
   center: {lat: 36.060574, lng: -107.961692},
   zoom: 18,
-  mapTypeId: 'satellite'
  });
+
+ google.maps.MapTypeId.satellite;
 
  var marker = new google.maps.Marker({
   position: center,

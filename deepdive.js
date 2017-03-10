9b8ec219c5b7c5991725e9275bd7a63d7935bb04
monyssharose <monyssharose@unm.edu>
2017-03-10 05:21:53 -0700

    Update deepdive.js

--- deepdive.js
+++ deepdive.js
@@ -5,8 +5,6 @@ function initMap(){
  var fountain = new google.maps.LatLng(41.900917,12.483337);
  var steps = new google.maps.LatLng(41.905989,12.482787);
  
- 
- 
  var map = new google.maps.Map(document.getElementById('map'), {
   center: {lat: 41.890333, lng: 12.491887},
   zoom: 13,
@@ -156,30 +154,18 @@ function initMap(){
 ]
 });
 
-
-
  var sistineContentString = '<div id="infoboxcontent">'+
   '<h3>Sistine Chapel</h3>'+
   '<p><i>Cappella Sistina</i></p>'+
   '<p>Latitude: 41.905, Longitude: 12.455</p>'+
   '<p><a href="https://en.wikipedia.org/wiki/Sistine_Chapel">https://en.wikipedia.org/wiki/Sistine_Chapel</a></p>'+
   '</div>';
-  
  var sistineInfoWindow = new google.maps.InfoWindow ({
   content: sistineContentString
   });
-
-  var sistineMarker = new google.maps.Marker({
-  position: sistine,
-  title: "Sistine Chapel"
-  });
- sistineMarker.setMap(map);
- 
  google.maps.event.addListener(sistineMarker, 'click', function() {
   sistineInfoWindow.open(map, sistineMarker);
   });
-  
- 
  
  var colosseumContentString = '<div id="infoboxcontent">'+
   '<h3>Colosseum</h3>'+
@@ -187,85 +173,76 @@ function initMap(){
   '<p>Latitude: 41.890, Longitude: 12.492</p>'+
   '<p><a href="https://en.wikipedia.org/wiki/Colosseum">https://en.wikipedia.org/wiki/Colosseum</a></p>'+
   '</div>';
-  
  var colosseumInfoWindow = new google.maps.InfoWindow ({
   content: colosseumContentString
   });
-  
- var colosseummarker = new google.maps.Marker({
-  position: colosseum,
-  title: "Colosseum"
-  });
- colosseummarker.setMap(map);
- 
  google.maps.event.addListener(colosseumMarker, 'click', function() {
   colosseumInfoWindow.open(map, colosseumMarker);
   });
- 
- 
+
  var pantheonContentString = '<div id="infoboxcontent">'+
   '<h3>Pantheon</h3>'+
   '<p><i>Pantheon</i></p>'+
   '<p>Latitude: 41.899, Longitude: 12.477</p>'+
   '<p><a href="https://en.wikipedia.org/wiki/Pantheon,_Rome">https://en.wikipedia.org/wiki/Pantheon,_Rome</a></p>'+
   '</div>';
-  
  var pantheonInfoWindow = new google.maps.InfoWindow ({
   content: pantheonContentString
   });
-  
- var pantheonmarker = new google.maps.Marker({
-  position: pantheon,
-  title: "Pantheon"
-  });
- pantheonmarker.setMap(map);
- 
  google.maps.event.addListener(pantheonMarker, 'click', function() {
   pantheonInfoWindow.open(map, pantheonMarker);
   });
- 
- 
+
  var fountainContentString = '<div id="infoboxcontent">'+
   '<h3>Trevi Fountain</h3>'+
   '<p><i>Fontana di Trevi</i></p>'+
   '<p>Latitude: 41.901, Longitude: 12.483</p>'+
   '<p><a href="https://en.wikipedia.org/wiki/Trevi_Fountain">https://en.wikipedia.org/wiki/Trevi_Fountain</a></p>'+
   '</div>';
-  
  var fountainInfoWindow = new google.maps.InfoWindow ({
   content: fountainContentString
   });
-  
- var fountainmarker = new google.maps.Marker({
-  position: fountain,
-  title: "Trevi Fountain"
-  });
- fountainmarker.setMap(map);
- 
  google.maps.event.addListener(fountainMarker, 'click', function() {
   fountainInfoWindow.open(map, fountainMarker);
   });
-  
-  
+ 
  var stepsContentString = '<div id="infoboxcontent">'+
   '<h3>Spanish Steps</h3>'+
   '<p><i>Scalinata di Trinita dei Monti</i></p>'+
   '<p>Latitude: 41.906, Longitude: 12.483</p>'+
   '<p><a href="https://en.wikipedia.org/wiki/Spanish_Steps">https://en.wikipedia.org/wiki/Spanish_Steps</a></p>'+
   '</div>';
-  
  var stepsInfoWindow = new google.maps.InfoWindow ({
   content: stepsContentString
   });
+ google.maps.event.addListener(stepsMarker, 'click', function() {
+  stepsInfoWindow.open(map, stepsMarker);
+  });
  
+ var sistineMarker = new google.maps.Marker({
+  position: sistine,
+  title: "Sistine Chapel"
+  });
+ sistineMarker.setMap(map);
+ var colosseummarker = new google.maps.Marker({
+  position: colosseum,
+  title: "Colosseum"
+  });
+ colosseummarker.setMap(map); 
+ var pantheonmarker = new google.maps.Marker({
+  position: pantheon,
+  title: "Pantheon"
+  });
+ pantheonmarker.setMap(map);
+ var fountainmarker = new google.maps.Marker({
+  position: fountain,
+  title: "Trevi Fountain"
+  });
+ fountainmarker.setMap(map);
  var stepsmarker = new google.maps.Marker({
   position: steps,
   title: "Spanish Steps"
   });
  stepsmarker.setMap(map);
  
- google.maps.event.addListener(stepsMarker, 'click', function() {
-  stepsInfoWindow.open(map, stepsMarker);
-  }); 
-
 }

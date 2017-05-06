31f0f51276fd15e6098aa6ca36ee518b77e0f651
monyssharose <monyssharose@unm.edu>
2017-05-06 14:50:50 -0600

    Update manhattan.js

--- manhattan.js
+++ manhattan.js
@@ -1,12 +1,48 @@
+var watercolor = new ol.layer.Group({
+	layers: [
+		new ol.layer.Tile({
+			source: new ol.source.Stamen({layer: 'watercolor'})
+		})
+	]
+});
+
+var toner = new ol.layer.Group({
+	layers: [
+		new.ol.layer.Tile({
+			source: new ol.source.Stamen({layer: 'toner'})
+		})
+	]
+});
+
+var terrain = new ol.layer.Group({
+	layers: [
+		new.ol.layer.Tile({
+			source: new ol.source.Stamen({layer: 'terrain'})
+		})
+	]
+});
+
+
 var myMap = new ol.Map({
 	target: 'map',
-	layers: [
-  		new ol.layer.Tile({
-            source: new ol.source.Stamen({layer: 'watercolor'})
-        })
-	],
 	view: new ol.View({
 		center: ol.proj.fromLonLat([-73.977,40.766]),
 		zoom: 12
 		})
 	});
+
+
+function setMapType(newType) {
+    if(newType == 'OSM') {
+        myMap.setLayerGroup(Layer_OSM);
+    } else if (newType == 'Watercolor') {
+        myMap.setLayerGroup(watercolor);
+    } else if (newType == 'Toner') {
+        myMap.setLayerGroup(toner);
+    } else if (newType == 'Terrain') {
+        myMap.setLayerGroup(terrain);
+	}
+}
+
+
+setMapType('watercolor')

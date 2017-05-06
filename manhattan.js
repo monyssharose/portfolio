517faf13cbc98214c571ed3dbdb08656b0e12a3d
monyssharose <monyssharose@unm.edu>
2017-05-05 19:37:48 -0600

    Update manhattan.js

--- manhattan.js
+++ manhattan.js
@@ -1,12 +1,11 @@
-var myMap = new ol.Map({
-	target: 'map',
+var map = new ol.Map({
 	layers: [
   		new ol.layer.Tile({
             source: new ol.source.OSM()
         })
 	],
 	view: new ol.View({
-		center: ol.proj.fromLonLat([40.766328,-73.977639]),
+		center: [40.766328,-73.977639],
 		zoom: 14
 		})
 	});

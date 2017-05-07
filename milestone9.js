f509cd9fdd636e49c14912d5c5c84af9147b1a69
monyssharose <monyssharose@unm.edu>
2017-05-06 18:09:14 -0600

    Create milestone9.js

---
+++ milestone9.js
@@ -0,0 +1,12 @@
+		var myMap = new ol.Map({
+			target: 'map',
+			layers: [
+  				new ol.layer.Tile({
+            				source: new ol.source.Stamen({layer: 'toner'})
+        			})
+			],
+			view: new ol.View({
+				center: ol.proj.fromLonLat([-73.977,40.766]),
+				zoom: 12
+			})
+		});

0464b226188d6253c0f3393097fc79a7861a8080
monyssharose <monyssharose@unm.edu>
2017-05-10 23:21:22 -0600

    Update final_q6.js

--- final_q6.js
+++ final_q6.js
@@ -1,11 +1,12 @@
 var myMap = new ol.Map({
 	target: 'map',
-  layers: [
-    new ol.layer.Tile({
-      source: new ol.source.Stamen({layer: 'terrain'})
-    })
-	view: new ol.View({
-		center: ol.proj.fromLonLat([-106.621694,35.095104]),
-		zoom: 12
+	layers: [
+		new ol.layer.Tile({
+			source: new ol.source.Stamen({layer: 'terrain'})
 		})
-	});
+	],
+	view: new ol.View({
+		center: ol.proj.fromLonLat([-106.629137,35.106808]),
+		zoom: 12
+	})
+});

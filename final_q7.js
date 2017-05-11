380e8524efa4ab7ff63999f96c6308790be7c557
monyssharose <monyssharose@unm.edu>
2017-05-11 03:01:49 -0600

    Update final_q7.js

--- final_q7.js
+++ final_q7.js
@@ -25,7 +25,7 @@ var raster_two = new ol.layer.Tile({
 
 var vector_one = new ol.layer.Tile({
 	source: new ol.source.TileWMS({
-		params: {'LAYERS':'monyssharose_ws:35106-A5_CONT'},
+		params: {'LAYERS':'monyssharose_ws:35106-A5_CONT_styled'},
 		url: 'http://mapper.internetmapping.net:8081/geoserver/monyssharose_ws/wms?',
 		serverType: 'geoserver',
 		projection: projection
@@ -34,7 +34,7 @@ var vector_one = new ol.layer.Tile({
 
 var vector_two = new ol.layer.Tile({
 	source: new ol.source.TileWMS({
-		params: {'LAYERS':'monyssharose_ws:35106-A6_CONT'},
+		params: {'LAYERS':'monyssharose_ws:35106-A6_CONT_styled'},
 		url: 'http://mapper.internetmapping.net:8081/geoserver/monyssharose_ws/wms?',
 		serverType: 'geoserver',
 		projection: projection
@@ -43,7 +43,7 @@ var vector_two = new ol.layer.Tile({
 
 var vector_three = new ol.layer.Tile({
 	source: new ol.source.TileWMS({
-		params: {'LAYERS':'monyssharose_ws:tgr2006se_bern_lka'},
+		params: {'LAYERS':'monyssharose_ws:tgr2006se_bern_lka_styled'},
 		url: 'http://mapper.internetmapping.net:8081/geoserver/monyssharose_ws/wms?',
 		serverType: 'geoserver',
 		projection: projection
@@ -54,7 +54,9 @@ var vector_three = new ol.layer.Tile({
 var myMap = new ol.Map({
 	target: 'map',
 	layers: [
-		raster_one,raster_two,vector_one,vector_two,vector_three
+		new ol.layer.Tile({
+			source: new ol.source.Stamen({layer: 'watercolor'})
+		}),raster_one,raster_two,vector_one,vector_two,vector_three
 	],
 	view: new ol.View({
 		center: ol.proj.fromLonLat([-106.586374,35.077869]),

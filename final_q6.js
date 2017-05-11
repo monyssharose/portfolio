09684e4cced9947e850e763b0c12b786306af0ef
monyssharose <monyssharose@unm.edu>
2017-05-11 00:51:07 -0600

    Update final_q6.js

--- final_q6.js
+++ final_q6.js
@@ -1,9 +1,50 @@
+var raster_one = new ol.layer.Image({
+	source: new ol.source.ImageWMS({
+		params: {'LAYERS':'monyssharose_ws:35106-A5'},
+		url: 'http://mapper.internetmapping.net:8081/geoserver/monyssharose/wms?',
+		serverType: 'geoserver'
+	})
+})
+
+var raster_two = new ol.layer.Image({
+	source: new ol.source.ImageWMS({
+		params: {'LAYERS':'monyssharose_ws:35106-A6'},
+		url: 'http://mapper.internetmapping.net:8081/geoserver/monyssharose/wms?',
+		serverType: 'geoserver'
+	})
+})
+
+var vector_one = new ol.layer.Tile({
+	source: new ol.source.TileWMS({
+		params: {'LAYERS':'monyssharose_ws:35106-A5_CONT'},
+		url: 'http://mapper.internetmapping.net:8081/geoserver/monyssharose_ws/wms?',
+		serverType: 'geoserver'
+	})
+})
+
+var vector_two = new ol.layer.Tile({
+	source: new ol.source.TileWMS({
+		params: {'LAYERS':'monyssharose_ws:35106-A6_CONT'},
+		url: 'http://mapper.internetmapping.net:8081/geoserver/monyssharose_ws/wms?',
+		serverType: 'geoserver'
+	})
+})
+
+var vector_three = new ol.layer.Tile({
+	source: new ol.source.TileWMS({
+		params: {'LAYERS':'monyssharose_ws:tgr2006se_bern_lka'},
+		url: 'http://mapper.internetmapping.net:8081/geoserver/monyssharose_ws/wms?',
+		serverType: 'geoserver'
+	})
+})
+
+
 var myMap = new ol.Map({
 	target: 'map',
 	layers: [
 		new ol.layer.Tile({
 			source: new ol.source.Stamen({layer: 'terrain'})
-		})
+		}),raster_one,raster_two,vector_one,vector_two,vector_three
 	],
 	view: new ol.View({
 		center: ol.proj.fromLonLat([-106.586374,35.077869]),
